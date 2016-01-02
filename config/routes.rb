c750e788918e4003c2134e0b7b102e05ad5876fc
jordifierro <jordifierromulero@gmail.com>
2016-01-02 23:31:43 +0100

    Set the api route constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+    # Api definition
+    namespace :api, defaults: { format: :json },
+                    constraints: { subdomain: 'api' }, path: '/' do
+      # We are going to list our resources here
+    end
 end
