6de7e97303d7e926a6c8927eec1455c2e4f2c648
jordifierro <jordifierromulero@gmail.com>
2016-01-03 00:39:06 +0100

    Add api_constraints and define route scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
-Rails.application.routes.draw do
-    # Api definition
-    namespace :api, defaults: { format: :json },
-                    constraints: { subdomain: 'api' }, path: '/' do
+require 'api_constraints.rb'
+
+RailsApiBase::Application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' }, path: '/' do
+
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
     end
+  end
 end
