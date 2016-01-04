54622228217f46e4d29755e1da6a6dcb92281a9e
jordifierro <jordifierromulero@gmail.com>
2016-01-04 14:55:20 +0100

    Add controller methods

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,8 +1,31 @@
 class Api::V1::NotesController < ApplicationController
 
   def index
-    @notes = Note.all
-    render json: @notes
+    render Note.all
+  end
+
+  def show
+    render Note.find(params[:id])
+  end
+
+  def create
+    note = Note.create(note_params)
+    render note
+  end
+
+  def update
+    note = Note.update(params[:id], note_params)
+    render note
+  end
+
+  def destroy
+    note = Note.find(params[:id])
+    note.destroy
+    head 204
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :content)
   end
 
 end
