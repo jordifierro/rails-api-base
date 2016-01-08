606169700ed48dc7ccaff04b03a7be1c347e48c2
jordifierro <jordifierromulero@gmail.com>
2016-01-08 18:59:13 +0100

    Fix broken tests from should prohibition

--- spec/lib/api_constraints_spec.rb
+++ spec/lib/api_constraints_spec.rb
@@ -9,12 +9,12 @@ describe ApiConstraints do
     it "returns true when the version matches the 'Accept' header" do
       request = double(host: 'api.railsapibase.dev',
                        headers: {"Accept" => "application/vnd.railsapibase.v1"})
-      api_constraints_v1.matches?(request).should be true
+      expect(api_constraints_v1.matches?(request)).to be true
     end
 
     it "returns the default version when 'default' option is specified" do
       request = double(host: 'api.railsapibase.dev')
-      api_constraints_v2.matches?(request).should be true
+      expect(api_constraints_v2.matches?(request)).to be true
     end
   end
 end
