4ebb6ef093aa1f3902b78d9388824c061c28d6c3
jordifierro <jordifierromulero@gmail.com>
2016-01-13 23:28:45 +0100

    Implement users controller create method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
