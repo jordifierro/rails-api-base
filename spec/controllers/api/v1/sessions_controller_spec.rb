c989facd20f3482e43352b17865353fb824e564f
jordifierro <jordifierromulero@gmail.com>
2016-01-14 14:50:04 +0100

    Define routes for sessions spec

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Api::V1::SessionsController do
-
+  it "routes correctly" do
+    expect(post: "/users/login").to route_to("api/v1/sessions#create", format: :json)
+    expect(delete: "/users/logout").to route_to("api/v1/sessions#destroy", format: :json)
+  end
 end
