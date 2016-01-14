ad85b74b1e4ce3195832f0c2152b504d0dc4af86
jordifierro <jordifierromulero@gmail.com>
2016-01-14 14:30:44 +0100

    Implement generate_auth_token method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          # :recoverable, :rememberable, :trackable,
 
   validates :auth_token, uniqueness: true
+  before_create :generate_auth_token!
+
+  def generate_auth_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
