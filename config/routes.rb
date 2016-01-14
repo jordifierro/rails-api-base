9a5fc11e6e98157a96ad8ab4ae02d8d28f1f0aaf
jordifierro <jordifierromulero@gmail.com>
2016-01-14 14:53:39 +0100

    Route login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ require 'api_constraints.rb'
 RailsApiBase::Application.routes.draw do
   # Api definition
   scope module: :api, defaults: { format: :json }  do
-    scope module: :v1,
-              constraints: ApiConstraints.new(version: 1, default: true) do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      post   'users/login'  => 'sessions#create'
+      delete 'users/logout' => 'sessions#destroy'
       resources :users, only: [:create, :destroy]
       resources :notes
     end
