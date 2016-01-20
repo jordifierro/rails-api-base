a901780bcf3ba3203e4e6fa1a28f97d4779e2d5b
jordifierro <jordifierromulero@gmail.com>
2016-01-20 17:26:45 +0100

    Test current_user function with wrong token

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -10,13 +10,26 @@ describe Authenticable do
   subject { authentication }
 
   describe "#current_user" do
-    before do
-      request.headers["Authorization"] = user.auth_token
-      authentication.stub(:request).and_return(request)
+    context "when correct auth" do
+      before do
+        request.headers["Authorization"] = user.auth_token
+        authentication.stub(:request).and_return(request)
+      end
+
+      it "returns the user from the authorization header" do
+        expect(authentication.current_user.auth_token).to eq user.auth_token
+      end
     end
 
-    it "returns the user from the authorization header" do
-      expect(authentication.current_user.auth_token).to eq user.auth_token
+    context "when wrong auth" do
+      before do
+        request.headers["Authorization"] = "wrong_token"
+        authentication.stub(:request).and_return(request)
+      end
+
+      it "returns the user from the authorization header" do
+        expect(authentication.current_user).to be_nil
+      end
     end
   end
 end
