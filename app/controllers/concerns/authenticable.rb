22e23e1cb58a3d948826a425e0dba265bfd5f01e
jordifierro <jordifierromulero@gmail.com>
2016-01-21 21:02:36 +0100

    Refactor authenticable methods

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -5,7 +5,11 @@ module Authenticable
     @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
   end
 
+  def user_signed_in?
+    current_user.present?
+  end
+
   def auth_with_token!
-    head :unauthorized unless current_user.present?
+    head :unauthorized unless user_signed_in?
   end
 end
