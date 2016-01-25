e1f8f02993e5a1cc1cc0b24c6c15d3b351bc03a4
jordifierro <jordifierromulero@gmail.com>
2016-01-25 22:04:20 +0100

    Define authenticable concern rspec with fake controller

---
+++ spec/controllers/api/v1/concerns/authenticable_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Api::V1::Concerns::Authenticable, type: :controller do
+  controller(Api::V1::ApiController) do
+    def fake_current_user
+      render json: current_user
+    end
+  end
+
+  before { routes.draw { get 'fake_current_user' => 'anonymous#fake_current_user' } }
+
+  context 'when correctly authenticated' do
+    before { signed_get :fake_current_user, nil }
+
+    it { expect(json_response['email']).to_not be_nil }
+    it { expect(response.status).to eq 200 }
+  end
+
+  context 'when not authenticated' do
+    before { get :fake_current_user, nil }
+
+    it { expect(response.status).to eq 401 }
+  end
+end
