4186a9e2f9d8e0823b1c92a5d85d45b95a7afedc
jordifierro <jordifierromulero@gmail.com>
2016-01-25 19:35:29 +0100

    Add user to note model and specs

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -6,10 +6,15 @@ describe Note do
   describe "responds to attrs" do
     it { expect(note).to respond_to(:title) }
     it { expect(note).to respond_to(:content) }
+    it { expect(note).to respond_to(:user) }
   end
 
   it "has a valid factory" do
     expect(note).to be_valid
+  end
+
+  it "creates a valid note" do
+    expect(note.save).to be true
   end
 
   it "is invalid without a title" do
@@ -18,11 +23,19 @@ describe Note do
 
   it "validates presence of title" do
     note = Note.new
+    note.user = create :user
     expect(note.valid?).to be false
     expect(note.errors.keys).to include(:title)
   end
 
-  it "creates a valid note" do
-    expect(note.save).to be true
+  it "is invalid without a user" do
+    expect(build(:note, user: nil)).to_not be_valid
+  end
+
+  it "validates presence of user" do
+    note = Note.new
+    note.title = "title"
+    expect(note.valid?).to be false
+    expect(note.errors.keys).to include(:user)
   end
 end
