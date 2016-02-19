c97aad8deea48294f65714779d1843f77ca5da11
jordifierro <jordifierromulero@gmail.com>
2016-02-19 19:52:26 +0100

    Refactor more files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 require 'api_constraints.rb'
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,
+  # see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
