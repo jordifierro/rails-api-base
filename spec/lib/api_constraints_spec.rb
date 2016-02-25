dba6c26d7389b6fd9c6c092e2b8f9002c275cfdc
jordifierro <jordifierromulero@gmail.com>
2016-02-26 00:14:41 +0100

    Refactor style api_constraints_spec.rb

--- spec/lib/api_constraints_spec.rb
+++ spec/lib/api_constraints_spec.rb
@@ -4,15 +4,15 @@ describe ApiConstraints do
   let(:api_constraints_v1) { ApiConstraints.new(version: 1) }
   let(:api_constraints_v2) { ApiConstraints.new(version: 2, default: true) }
 
-  describe "matches?" do
-
-    it "returns true when the version matches the 'Accept' header" do
+  describe 'matches?' do
+    it 'returns true when the version matches the \'Accept\' header' do
       request = double(host: 'api.railsapibase.dev',
-                       headers: {"Accept" => "application/vnd.railsapibase.v1"})
+                       headers:
+                         { 'Accept' => 'application/vnd.railsapibase.v1' })
       expect(api_constraints_v1.matches?(request)).to be true
     end
 
-    it "returns the default version when 'default' option is specified" do
+    it 'returns the default version when \'default\' option is specified' do
       request = double(host: 'api.railsapibase.dev')
       expect(api_constraints_v2.matches?(request)).to be true
     end
