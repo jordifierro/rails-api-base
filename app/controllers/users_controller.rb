0324538e912d876806d70e8552c9b209438a500e
jordifierro <jordifierromulero@gmail.com>
2016-04-16 16:46:00 +0200

    Test users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ActionController::Base
   def confirm
-    user = User.find_by_confirmation_token(params[:token])
+    user = User.find_by_conf_token(params[:token])
     if user
       user.conf_at = DateTime.current
       user.conf_token = nil
       user.save
-      render 'users/confirmed'
+      render '/users/confirmed'
     else
       render 'users/invalid_confirmation'
     end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+RSpec.configure do |config|
+  config.render_views = true
+end
+
+describe UsersController do
+  let(:user) { create :user }
+  let(:user_attr) { attributes_for :user }
+
+  it 'routes correctly' do
+    expect(get: '/users/confirm/3').to route_to('users#confirm', token: '3')
+  end
+
+  describe 'GET /users/confirm #confirm' do
+    context 'when is confirmed successfully' do
+      before(:each) do
+        user.conf_sent_at = DateTime.current
+        user.regenerate_conf_token
+        user.save
+        get :confirm, params: { token: user.conf_token }
+      end
+
+      it 'renders users/confirmed view' do
+        expect(response.body).to render_template('users/confirmed')
+      end
+
+      it 'sets conf_at attribute' do
+        user.reload
+        expect(user.conf_at).to_not be_nil
+      end
+    end
+
+    context 'when token is invalid' do
+      before(:each) { get :confirm, params: { token: 'WRONG_TOKEN' } }
+
+      it 'renders users/confirmed view' do
+        expect(response.body).to render_template('users/invalid_confirmation')
+      end
+
+      it 'doesn\'t set conf_at attribute' do
+        user.reload
+        expect(user.conf_at).to be_nil
+      end
+    end
+  end
+end
