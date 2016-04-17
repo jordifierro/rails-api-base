bfaa76759abf70f348d77dbfe05745cf497b0141
jordifierro <jordifierromulero@gmail.com>
2016-04-17 02:08:38 +0200

    Test mailer multipart

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe UserMailer do
+  def get_message_part(mail, content_type)
+    mail.body.parts.find do |p|
+      p.content_type.match content_type
+    end.body.raw_source
+  end
+
   describe 'UserMailer' do
     let(:user) { create :user }
     let(:mail) { UserMailer.ask_email_confirmation(user) }
@@ -13,10 +19,24 @@ describe UserMailer do
       expect(mail.subject).to eq I18n.t('email_confirmation.subject')
     end
 
-    it 'renders email_confirmation.ask with url' do
-      expect(mail.body.encoded).to match(
-        I18n.t('email_confirmation.ask',
-               confirm_url: users_confirm_url(user.conf_token)))
+    it 'renders email_confirmation.ask and url on plain' do
+      expect(mail.text_part.body.encoded).to match(
+        I18n.t('email_confirmation.ask'))
+      expect(mail.text_part.body.encoded).to match(
+        users_confirm_url(user.conf_token))
+    end
+
+    it 'renders email_confirmation.ask, url and on html' do
+      expect(mail.html_part.body.encoded).to match(
+        I18n.t('email_confirmation.ask'))
+      expect(mail.html_part.body.encoded).to match(
+        '<a href="' + users_confirm_url(user.conf_token))
+    end
+
+    it 'generates a multipart message (plain text and html)' do
+      expect(mail.body.parts.length).to eq(2)
+      expect(mail.body.parts.collect(&:content_type)).to match(
+        ['text/plain; charset=UTF-8', 'text/html; charset=UTF-8'])
     end
   end
 end
